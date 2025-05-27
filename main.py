5b46f2d8a2cf53325f4486194d29730aee7d92e1
Jenifer Urbano <jdurbanocordoba@gmail.com>
2025-05-27 08:43:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python3
+__author__ = "Jenifer U. C. Andres B. C. , Zara C. B."
+__version__ = "1.0.0"
+__license__ = "GPL"
+__email__ = "jenifer.urbano@campusucc.edu.co"
+
 """
 Juego del Ahorcado - Versión por Consola
 """
